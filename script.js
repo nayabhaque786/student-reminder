fc04f12a9cc14deb32c12b6cc723a50e9bd49608
Nayab Nehal Haque <nayabhaque786@gmail.com>
2026-09-09 18:33:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -289,9 +289,6 @@ function appendMessage(sender, text) {
   chatWindow.scrollTop = chatWindow.scrollHeight;
 }
 
-function setTheme(theme) {
-  document.body.className = theme + "-theme";
-}
 
 function sendMessage() {
   const input = document.getElementById("chatInput");
